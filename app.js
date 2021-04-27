1e554f6ab634a19889a639f387b34d00e5055d13
Rayvhen Mico Rosima <rayvhen_rosima@dlsu.edu.ph>
2021-04-27 22:45:26 +0800

    import express session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const session = require('express-session');
 
 const app = express(); //initializing express server
 

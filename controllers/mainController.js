a404e9895e8817cbfd48e4733502d4a726ce707a
Matthew Buensalida <matthew_buensalida@dlsu.edu.ph>
2021-04-29 15:08:13 +0800

    touches

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -68,17 +68,6 @@ const mainController = {
 
     },
 
-    getSettingsPage: function (req, res){
-
-        db.findOne(userCollection, {uName: req.session.uName},'',function (result){
-            res.render('settings',{
-                title: 'Settings',
-                css: ['global','settings'],
-                user: result
-            });
-        });
-    },
-
     //get picture
     getPicture: function(req, res) {
         var id = mongoose.Types.ObjectId(req.params.id);

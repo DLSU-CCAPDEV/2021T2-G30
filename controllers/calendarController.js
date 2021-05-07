bf50e035d7b0f421e710369a7de9934c68add0ec
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2021-05-07 22:40:01 +0800

    when user requests for calendar

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -2,7 +2,26 @@ const db = require('../models/db.js');
 const entryCollection = require('../models/EntryModel.js');
 
 const calendarController = {
-    
+    getCalendar: function(req, res) {
+        if(req.session.uName) {
+            res.render('calendar', {
+                title: 'Calendar View',
+                calendar:true,
+                css: ['global']
+            });
+        } else {
+            res.status(401);
+            res.render('error', {
+                title: '401 Unauthorized Access',
+                css:['global', 'error'],
+                status: {
+                    code: "401",
+                    message: "Unauthorized access."
+                } 
+                
+            });
+        }
+    }
 }
 
 module.exports = calendarController;

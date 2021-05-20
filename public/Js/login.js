81b10fa1e3a3d04adec8dcf77d4b5a1b5aac0430
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2021-05-20 22:53:40 +0800

    login button changed to submit

--- public/Js/login.js
+++ public/Js/login.js
@@ -1,45 +1,19 @@
 $(document).ready(function () {
     
-    $('#loginBtn').click(function () {
+    $('#loginForm').submit(function (event) {
+        event.preventDefault(); //avoid to execute submitting of the form
 
         // get the value entered the user in the `<input>` element
-        var uName = $('#LoginUsername').val();
+        var uName = $('#LoginUsername').val().trim();
         var pw = $('#LoginPassword').val();
         
-        /*
-            send an HTTP GET request using JQuery AJAX
-            the first parameter is the path in our server
-            which is defined in `../../routes/routes.js`
-            the server will execute the function getCheckID()
-            defined in `../../controllers/signupController.js`
-            the second parameter passes the variable `idNum`
-            as the value of the field `idNum`
-            to the server
-            the last parameter executes a callback function
-            when the server sent a response
-        */
         $.get('/checklogin', {uName: uName, pw: pw}, function (isValid) {
 
-            /*
-                if the current value of `idNum` exists in the database
-                change the background-color of the `<input>` element to red
-                display an error message
-                and disable the submit button
-            */
-            
             if(!isValid) {
                 $('#error').text('Invalid Login Credentials');
                 $('#error').css('margin-top', '-8px').css('margin-bottom', '8px');
-            }
-
-            /*
-                else
-                change the background-color of the `<input>` element back
-                remove the error message
-                and enable the submit button
-            */
-            else {
-                $('#loginForm').submit();
+            } else {
+                $('#loginForm').unbind().submit();
             }
         });
     });
